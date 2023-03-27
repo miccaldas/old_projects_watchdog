dcd36f26d92177befc669c86d8c3a7015b122310
mclds <mclds@protonmail.com>
2023-03-28 00:00:03 +0100

    28-03-2023 commit

--- old_projects_watchdog/file_checker.py
+++ old_projects_watchdog/file_checker.py
@@ -5,32 +5,26 @@ and they're part of a blacklist of spurious files, mainly git
 and logs, cache, that sort of thing, they'll be deleted.
 """
 import os
-import pickle
-from os import walk
+import shutil
 
-import snoop
-from db_decorator.db_information import db_information
-
-# import subprocess
-from rocketry import Rocketry
-from rocketry.conds import hourly
-from snoop import pp
+# import snoop
+# from snoop import pp
 
 
-def type_watch(source, value):
-    return "type({})".format(source), type(value)
+# def type_watch(source, value):
+#   return "type({})".format(source), type(value)
 
 
-snoop.install(watch_extras=[type_watch])
-
-app = Rocketry()
+# snoop.install(watch_extras=[type_watch])
 
 
-# @app.task(hourly)
-@db_information
-@snoop
+# @snoop
 def file_checker():
-    """"""
+    """
+    Checks if folder has files or subfolders in to delete lists.
+    Is called hourly by a cron job with root permissions.
+    """
+
     del_dir = [".git", "logs", "build", "__pycache__"]
     del_file = [
         ".gitignore",
@@ -39,20 +33,33 @@ def file_checker():
         "MANIFEST.in",
         "pyproject.toml",
         "setup.py",
-        "setup.cfg",
         "__init__.py",
     ]
     pth = "/home/mic/python/old_alternative_projects"
 
     folder_content = []
-    for root, dirs, files in walk(pth):
-        for file in files:
+    # os.walk will look in all sublevels of the dir tree for files or folders.
+    for root, dirs, files in os.walk(pth):
+        for file in files + dirs:
             folder_content.append(os.path.join(root, file))
 
-    with open("snapshot.bin", "wb") as f:
-        pickle.dump(folder_content, f)
+    # 'del_dir' and 'del_files' list only names not paths. As folder_content collects paths,
+    # we have to use 'os.path.basename(os.path.normpath())' to get only the names of files and folders.
+    deletes = [
+        i
+        for i in folder_content
+        if os.path.basename(os.path.normpath(i)) in del_dir
+        or os.path.basename(os.path.normpath(i)) in del_file
+    ]
+
+    if deletes != []:
+        for d in deletes:
+            if os.path.isdir(d):
+                # Only 'shutil.rmtree()', in python, deletes folders with files in it.
+                shutil.rmtree(d)
+            else:
+                os.remove(d)
 
 
 if __name__ == "__main__":
     file_checker()
-    # app.run()
